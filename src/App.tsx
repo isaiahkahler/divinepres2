db544e92f1dfc024814b1ed5d47eab9dbdf1bc70
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-12-23 16:49:07 -0500

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/home';
+import Create from './pages/create';
+import Present from './pages/present';
+import {HomeRoute, CreateRoute, PresentRoute} from './components/constants';
 
 function App(props: {}) {
   return(
-    <div>
-      app!
-    </div>
+    <Router>
+      <Switch>
+        <Route path={CreateRoute}>
+          <Create />
+        </Route>
+        <Route path={PresentRoute}>
+          <Present />
+        </Route>
+        <Route path={HomeRoute}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
