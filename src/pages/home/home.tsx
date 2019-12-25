b601facf90ad1e53626ce31fde7648783a4b5a2b
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-12-24 21:37:25 -0500

    home page UI

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import { Presentation } from '../../components/types';
+import { Container, Fab, Typography, Grid, styled, Theme, Box, useTheme } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import PlayIconOutlined from '@material-ui/icons/PlayCircleFilledOutlined';
+import AddIconOutlined from '@material-ui/icons/AddCircle';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import FullButton from '../../components/fullbutton';
 
 interface HomeProps {
-
+    presentations: Presentation[],
 }
 
-export default function Home(props:HomeProps) {
-    return(
-        <div>
-            Home
-        </div>
+
+const StyledFab = styled(Fab)((props: { theme: Theme }) => ({
+    position: 'absolute',
+    bottom: props.theme.spacing(2),
+    right: props.theme.spacing(2)
+}));
+
+
+export default function Home(props: HomeProps) {
+
+    const theme = useTheme();
+
+    return (
+        <Container maxWidth='md'>
+            <Grid container justify='center'>
+                <Typography variant='h1'><Box fontWeight='fontWeightBold' fontStyle='italic'>DivinePres 2</Box></Typography>
+                <Link to='/create'>
+                    <FullButton icon={AddIconOutlined}>
+                        create a new presentation
+                    </FullButton>
+                </Link>
+                {props.presentations.map((presentation, index) => <FullButton icon={PlayIconOutlined} key={index} isSecondary>
+                    presentation from {moment(presentation.timestamp).format('MM/DD/YY')}
+                </FullButton>)}
+            </Grid>
+            <Link to='/create'>
+            <StyledFab color='primary'>
+                <AddIcon />
+            </StyledFab>
+            </Link>
+        </Container>
     );
 }
