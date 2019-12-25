cd5a869d4bbb02e20dc8ea3457ce127afa434411
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-12-24 21:37:38 -0500

    custom theme variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,43 @@ import Home from './pages/home';
 import Create from './pages/create';
 import Present from './pages/present';
 import { HomeRoute, CreateRoute, PresentRoute } from './components/constants';
+import { createMuiTheme, Theme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: 'Arial'
+    },
+    palette: {
+        primary: {
+            main: "#ffbb00",
+            light: "#ffd86b",
+            dark: "#cc8708"
+        },
+        secondary: {
+            main: "#0077d9",
+            light: "#2ea1ff",
+            dark: "#004a87"
+        }
+    }
+});
 
 function App(props: {}) {
     return (
         <Router>
-            <Switch>
-                <Route path={CreateRoute}>
-                    <Create />
-                </Route>
-                <Route path={PresentRoute}>
-                    <Present />
-                </Route>
-                <Route path={HomeRoute}>
-                    <Home />
-                </Route>
-            </Switch>
+            <ThemeProvider theme={theme}>
+
+                <Switch>
+                    <Route path={CreateRoute}>
+                        <Create />
+                    </Route>
+                    <Route path={PresentRoute}>
+                        <Present />
+                    </Route>
+                    <Route path={HomeRoute}>
+                        <Home />
+                    </Route>
+                </Switch>
+            </ThemeProvider>
         </Router>
     );
 }
