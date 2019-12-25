f4e508811456404abb3e4b62ba24dc88ce5b149e
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-12-24 22:21:42 -0500

    cleanup on aisle home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Presentation } from '../../components/types';
-import { Container, Fab, Typography, Grid, styled, Theme, Box, useTheme } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { styled, Theme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import AddIcon from '@material-ui/icons/Add';
-import PlayIconOutlined from '@material-ui/icons/PlayCircleFilledOutlined';
-import AddIconOutlined from '@material-ui/icons/AddCircle';
+import PlayIcon from '@material-ui/icons/PlayCircleFilledOutlined';
+import AddIconCircle from '@material-ui/icons/AddCircle';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
-import FullButton from '../../components/fullbutton';
+import FullButton from '../../components/fullButton';
 
 interface HomeProps {
     presentations: Presentation[],
@@ -21,26 +26,24 @@ const StyledFab = styled(Fab)((props: { theme: Theme }) => ({
 
 
 export default function Home(props: HomeProps) {
-
-    const theme = useTheme();
-
     return (
         <Container maxWidth='md'>
-            <Grid container justify='center'>
-                <Typography variant='h1'><Box fontWeight='fontWeightBold' fontStyle='italic'>DivinePres 2</Box></Typography>
+            <Grid container justify='center' direction='column'>
+                <Typography variant='h1'><Box fontWeight='fontWeightBold' fontStyle='italic' textAlign='center'>DivinePres 2</Box></Typography>
+
                 <Link to='/create'>
-                    <FullButton icon={AddIconOutlined}>
+                    <FullButton icon={AddIconCircle}>
                         create a new presentation
                     </FullButton>
                 </Link>
-                {props.presentations.map((presentation, index) => <FullButton icon={PlayIconOutlined} key={index} isSecondary>
+                {props.presentations.map((presentation, index) => <FullButton icon={PlayIcon} key={index} isSecondary>
                     presentation from {moment(presentation.timestamp).format('MM/DD/YY')}
                 </FullButton>)}
             </Grid>
             <Link to='/create'>
-            <StyledFab color='primary'>
-                <AddIcon />
-            </StyledFab>
+                <StyledFab color='primary'>
+                    <AddIcon />
+                </StyledFab>
             </Link>
         </Container>
     );
