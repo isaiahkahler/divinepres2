6d955ed875ecd52621a8c50702f1777b5d9af95b
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-12-24 21:38:05 -0500

    home container

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './home';
+import { getStored } from '../../components/storage';
+import { Presentation } from '../../components/types';
+
 
 
 export default function HomeContainer(props: {}) {
+
+    const [presentations, setPresentations] = useState<Presentation[]>([]);
+
+    useEffect(() => {
+        setPresentations(getStored());
+    }, []);
+
     return(
-        <Home />
+        <Home presentations={presentations} />
     );
 }
